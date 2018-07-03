25d0c7aa5c7093bc2f5951d3154db9e958fbe415
hzl091 <hzl091@126.com>
2018-07-03 23:21:49 +0800

    添加列表展示页

--- 2-Client/DC.Web/Controllers/TableController.cs
+++ 2-Client/DC.Web/Controllers/TableController.cs
@@ -8,6 +8,7 @@ using DC.Data.Request.DataManage;
 using DC.DAL;
 using DC.IService.DataManage;
 using DC.Service.DataManage;
+using MyFX.Core.BaseModel.Result;
 using MyFX.Core.DI;
 using MyFX.Core.Domain.Uow;
 using MyFX.Core.Logs;
@@ -27,9 +28,9 @@ namespace DC.Web.Controllers
         // GET: Table
         public ActionResult Index()
         {
-            var tables = _tableInfoService.FindTables(new FindTablesRequest());
-            return Content(tables.Data.First().Name);
-            //return View();
+            var res = _tableInfoService.FindTables(new FindTablesRequest());
+            res.CheckErrorAndThrowIt();
+            return View(res.Data);
         }
 
         public ActionResult Detail(int id, string orderBy = "ID", string sort = "DESC", int pageIndex = 1)
